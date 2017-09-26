7d02d18046b08bca55d8a1ed1b3e0d0acbf2c223
张全成 <625299914@qq.com>
2017-09-26 11:25:03 +0800

    test

---
+++ src/main/java/com/zqc/SchoolCourseApplication.java
@@ -0,0 +1,12 @@
+package com.zqc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolCourseApplication.class, args);
+	}
+}
